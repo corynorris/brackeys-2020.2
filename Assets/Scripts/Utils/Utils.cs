61e69be67b6dc992df64baeea6e602a660e630bd
Artom Didinchuk <didinchukar@gmail.com>
2021-08-22 12:11:24 -0400

    Resolving utils issue

--- Assets/Scripts/Utils/Utils.cs
+++ Assets/Scripts/Utils/Utils.cs
@@ -9,4 +9,47 @@ public class Utils
     {
         return UnityEngine.Random.insideUnitCircle.normalized;
     }
+
+    public static AudioSource AddAudioNoFalloff(GameObject gameObject, AudioClip clip, bool loop, bool playAwake, float vol, float pitch, float minRange = 100, float maxRange = 100)
+    {
+        AudioSource newAudio = gameObject.AddComponent<AudioSource>();
+        newAudio.clip = clip;
+        newAudio.loop = loop;
+        newAudio.playOnAwake = playAwake;
+        newAudio.volume = vol;
+        newAudio.pitch = pitch;
+        newAudio.spatialBlend = 1f;
+        newAudio.rolloffMode = AudioRolloffMode.Linear;
+        newAudio.minDistance = minRange;
+        newAudio.maxDistance = maxRange;
+
+        return newAudio;
+    }
+
+    public static float GetAngleFromVector(Vector3 vector)
+    {
+        float radians = Mathf.Atan2(vector.y, vector.x);
+        float degrees = radians * Mathf.Rad2Deg;
+        return degrees;
+    }
+
+    public static IEnumerator StartFade(AudioSource audioSource, float duration, float targetVolume)
+    {
+        float currentTime = 0;
+        float start = audioSource.volume;
+
+        while (currentTime < duration)
+        {
+            currentTime += Time.deltaTime;
+            audioSource.volume = Mathf.Lerp(start, targetVolume, currentTime / duration);
+            yield return null;
+        }
+
+        if (targetVolume == 0)
+        {
+            audioSource.Stop();
+        }
+
+        yield break;
+    }
 }
